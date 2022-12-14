f58167c02792dac581358f712f5060c8b58b7ea4
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-12-14 16:58:50 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.6"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "in.tech-camp"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
